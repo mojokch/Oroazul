4d465df12df6646a04f57b9750e0bcb414315bec
mojokch <84201242+mojokch@users.noreply.github.com>
2021-05-22 16:25:14 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sanitizantes-Oro Azul</title>
+	<meta charset="utf-8">
+	<meta name="description" content="Producto 100% natural elaborado con nanopartículas de iones de cobre y agua carbonizada. Amigable con el medio ambiente y ser 
+	humano (no tóxico).">
+	<meta name="keywords" content="Sanitizante,uso medico,iones,calidad,cobre,aguascalientes,méxico,mex,mexico,ags,limpieza,desinfectante,natural,grado,alimenticio,cofepris">
+	<meta name="robots" content="index, follow">
+	<meta name="language" content="ES">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="img/logo1.png">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="https://kit.fontawesome.com/37ef4fca07.js" crossorigin="anonymous"></script>	
+</head>
+<body>
+	<header>
+		<div class="container">
+				<a href="index.html">
+					<img src="img/logo1.png" class="logo">
+				</a>
+				<nav class="nav1">
+					<a href="#inicio">Inicio</a>
+					<a href="#nosotros">Nuestros Productos</a>
+					<a href="#clientes">Clientes</a>
+					<a href="#contactenos">Contáctenos</a>
+				</nav>
+			</div>
+	</header>
+	<main>
+		<div class="slider">
+				<ul>
+					<li><img class="ajuste-perfecto" src="img/usos (1).png" alt=""></li>
+					<li><img class="ajuste-perfecto" src="img/usos (2).png" alt=""></li>
+					<li><img class="ajuste-perfecto" src="img/usos (8).png" alt=""></li>
+					<li><img class="ajuste-perfecto" src="img/usos (9).png" alt=""></li>
+				</ul>
+
+		</div>
+		<section id="inicio" class="inicio">
+			<div class="bloque-inicio">
+					<img class="logo-gota" src="img/logo-gota.png">
+					<h1>ORO <b>AZUL</b></h1>
+					<p>IONES DE COBRE
+					</p>
+					<a href="#nosotros" class="boton boton-1">Ver más</a>
+			</div>				
+		</section>
+		<section id="nosotros" class="nosotros">
+			<div>
+			<img class="svglogo" src="img/svg.png">
+			<h2>Nuestra tecnologia SVG ®;</h2>
+			<p>SVG®: Iones de cobre, brinda una protección real, efectiva, no dañina y es el ingrediente activo en todos los productos Oro Azul®.
+			Los iones de cobre son capaz de neutralizar virus, bacterias, hongos, tripanosomas y mico bacterias, sin ser corrosivo, sin causar daño o tener efectos secundarios, es biodegradable, de  amplio espectro, y puede alcanzar una residualidad de hasta 72 hrs.</p>
+			</div>
+		</section>
+		<section id="clientes" class="clientes">
+			<div class="container">
+				<h2>Clientes </h2>
+				<div class="row">
+					<div class="clientes-container">
+						<img src="img/cliente (1).png">						
+					</div>
+					<div class="clientes-container">
+						<img src="img/cliente (2).png">						
+					</div>
+					<div class="clientes-container">
+						<img src="img/cliente (3).png">						
+					</div>
+					<div class="clientes-container">
+						<img src="img/cliente (4).png">						
+					</div>
+					<div class="clientes-container">
+						<img src="img/cliente (5).png">						
+					</div>
+					<div class="clientes-container">
+						<img src="img/cliente (6).png">						
+					</div>
+					<div class="clientes-container">
+						<img src="img/cliente (7).png">						
+					</div>
+					<div class="clientes-container">
+						<img src="img/cliente (8).png">						
+					</div>
+					<div class="clientes-container">
+						<img src="img/cliente (9).png">						
+					</div>
+					<div class="clientes-container">
+						<img src="img/cliente (10).png">						
+					</div>
+					
+				</div>
+				
+			</div>
+			
+		</section>
+		<section id="contactenos" class="contactenos">
+			
+		</section>
+	</main>
+	<footer>
+					<div class="columna columna-25 columna-mobile-100">
+						<h3>
+							Redes Sociales
+						</h3>
+						<ul class="redes">
+							<li>
+								<a href="https://es-la.facebook.com/mojokch" target="_blank" class="face">
+									<i class="fab fa-facebook-square"></i>
+								</a>
+							</li>
+							<li>
+								<a href="https://twitter.com/mojokch" target="_blank" class="twit">
+									<i class="fab fa-twitter-square"></i>
+								</a>
+							</li>
+							<li>
+								<a href="https://api.whatsapp.com/send?phone=524499083119&text=Hola%20este%20mensaje%20es%20de%20prueba%20" target="_blank" class="whats">
+									<i class="fab fa-whatsapp-square"></i>
+									</a>
+							</li>
+						</ul>
+					</div>
+	</footer>
+
+</body>
+</html>
